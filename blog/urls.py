4e6c9cb84c41a7d94e3d42a25cbcd02b062c49eb
József Mandula <63676153+joealmond@users.noreply.github.com>
2022-02-18 11:58:06 +0100

    my solution for chapter 55 project task

--- blog/urls.py
+++ blog/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from blog import views
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include("blog.urls")),
+    path("", views.index, name="index"),
+    path("blog/", views.posts, name="posts"),
+    path("blog/<int:post>", views.blog_post_by_number),
+    path("blog/<str:post>", views.blog_post, name="post_page")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
+from django.http import Http404, HttpResponseRedirect, HttpResponseNotFound, HttpResponse
+from django.urls import reverse
+
+import blog
+
+
+blog_posts = {
+    "first": "My first blogpost...",
+    "second": "My second blogpost...",
+    "third": "My third blogpost..."
+}
+
 
 # Create your views here.
+
+def index(request):
+    post_list = list(blog_posts.keys())
+    last_two_post = post_list[-2:]
+    last_two_post.reverse()
+    return render(request, "blog/index.html", {
+        "last_two_post": last_two_post
+    })
+
+
+def posts(request):
+    post_list = list(blog_posts.keys())
+    post_list.reverse()
+    return render(request, "blog/posts.html", {
+        "post_list": post_list
+    })
+
+
+def blog_post_by_number(request, post):
+    post_list = list(blog_posts.keys())
+
+    if post > len(post_list):
+        return HttpResponseNotFound("Invalid post.")
+
+    redirect_post = post_list[post - 1]
+    redirect_path = reverse(
+        "post_page", args=[redirect_post])
+    return HttpResponseRedirect(redirect_path)
+
+
+def blog_post(request, post):
+    post_text = blog_posts[post]
+    return render(request, "blog/post.html", {
+        "title": post,
+        "post": post,
+        "text": post_text
+    })
